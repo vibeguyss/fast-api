eeaaaf03e13b35ce17d057bfbdf41e6bec320857
ganggun <165283835+ganggun@users.noreply.github.com>
2025-07-16 01:43:56 +0900

    add

--- back-end/src/main/java/com/learnmore/legacy/domain/user/error/UserError.java
+++ back-end/src/main/java/com/learnmore/legacy/domain/user/error/UserError.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserError implements CustomError {
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다."),
-    ID_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 사용중인 id 입니다.");
+    ID_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 사용중인 id 입니다."),
+    PW(HttpStatus.BAD_REQUEST, "비번틀림");
 
     private final HttpStatus status;
     private final String message;
